f0dfeffb82d06dab38a9ccf160142de003fbfda5
liyuan.zhao <liyuan.zhao@thundersoft.com>
2022-04-28 13:49:23 +0800

    zhaoliyuan review

--- app/src/main/java/com/example/applicationtext2/MainActivity.java
+++ app/src/main/java/com/example/applicationtext2/MainActivity.java
@@ -10,15 +10,16 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText mUsernumber,mPwd;
-    private Button login,register;
-    private static final String LOGINNUM = "17641241979";
-    private static final String LOGINPWD = "521314";
+    private EditText mUserNumber, mPwd;
+    private Button login, register;
+    private static final String LOGIN_NUM = "17641241979";
+    private static final String LOGIN_PWD = "521314";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mUsernumber = findViewById(R.id.editText1);
+        mUserNumber = findViewById(R.id.editText1);
         mPwd = findViewById(R.id.editText2);
         login = findViewById(R.id.button1);
         register = findViewById(R.id.button2);
@@ -26,15 +27,17 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mUsernumber.getText().toString().equals(LOGINNUM) && mPwd.getText().toString().equals(LOGINPWD)){
-                    Toast.makeText(MainActivity.this,"登录成功！",Toast.LENGTH_SHORT).show();
+                if (mUserNumber.getText().toString().equals(LOGIN_NUM) && mPwd.getText().toString()
+                        .equals(LOGIN_PWD)) {
+                    Toast.makeText(MainActivity.this, "登录成功！", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent();
-                    intent.setClass(MainActivity.this , MyToolAcyivity.class);
-                    intent.putExtra("userNumber" , mUsernumber.getText().toString());
-                    intent.putExtra("pwd" , mPwd.getText().toString());
+                    intent.setClass(MainActivity.this, MyToolActivity.class);
+                    intent.putExtra("userNumber", mUserNumber.getText().toString());
+                    intent.putExtra("pwd", mPwd.getText().toString());
                     startActivity(intent);
-                }else {
-                    Toast.makeText(MainActivity.this,"登录失败，账号或者密码错误！",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "登录失败，账号或者密码错误！",
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- app/src/main/java/com/example/applicationtext2/MyToolActivity.java
+++ app/src/main/java/com/example/applicationtext2/MyToolActivity.java
@@ -7,29 +7,31 @@ import android.os.Message;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MyToolAcyivity<liv> extends AppCompatActivity {
+public class MyToolActivity extends AppCompatActivity {
+
+    private static final int MSG_UPDATE_PROGRESS = 0x111;
+    private static final int MSG_FINISH_PROGRESS = 0x110;
+    private static final int PROGRESS_MAX_VALUE = 100;
+    private static final int PROGRESS_DELAY_VALUE = 200;
     private ProgressBar mHorizonP;
     private int mProgressStatus = 0;
     private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        List<Integer> imageId = new ArrayList<Integer>();
+        List<Integer> imageId = new ArrayList<>();
         imageId.add(R.drawable.img01);
         imageId.add(R.drawable.img02);
         imageId.add(R.drawable.img03);
@@ -38,13 +40,13 @@ public class MyToolAcyivity<liv> extends AppCompatActivity {
         setContentView(R.layout.item_activity);
         mHorizonP = (ProgressBar) findViewById(R.id.progress1);
 
-        mHandler = new Handler(){
+        mHandler = new Handler() {
             @Override
-            public void handleMessage(Message msg){
-                if(msg.what==0x111){
+            public void handleMessage(Message msg) {
+                if (msg.what == MSG_UPDATE_PROGRESS) {
                     mHorizonP.setProgress(mProgressStatus);
-                }else {
-                    Toast.makeText(MyToolAcyivity.this, "耗时已经完成",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MyToolActivity.this, "耗时已经完成", Toast.LENGTH_SHORT).show();
                     mHorizonP.setVisibility(View.GONE);
                     GridView gridView = (GridView) findViewById(R.id.gridview1);
                     BaseAdapter adapter = new BaseAdapter() {
@@ -65,14 +67,13 @@ public class MyToolAcyivity<liv> extends AppCompatActivity {
 
                         @Override
                         public View getView(int position, View convertView, ViewGroup parent) {
-
                             ImageView imageView;
-                            if(convertView==null){
-                                imageView = new ImageView(MyToolAcyivity.this);
+                            if (convertView == null) {
+                                imageView = new ImageView(MyToolActivity.this);
                                 imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-                                imageView.setPadding(5,0,5,0);
-                            }else {
-                                imageView=(ImageView) convertView;
+                                imageView.setPadding(5, 0, 5, 0);
+                            } else {
+                                imageView = (ImageView) convertView;
                             }
                             imageView.setImageResource(imageId.get(position));
                             return imageView;
@@ -81,29 +82,33 @@ public class MyToolAcyivity<liv> extends AppCompatActivity {
                     gridView.setAdapter(adapter);
                     gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
                         @Override
-                        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-
-                            AlertDialog alert = new AlertDialog.Builder(MyToolAcyivity.this).create();
+                        public boolean onItemLongClick(AdapterView<?> parent, View view,
+                                                       int position, long id) {
+                            AlertDialog alert = new AlertDialog.Builder(MyToolActivity.this).create();
                             alert.setIcon(R.drawable.img05);
                             alert.setTitle("系统提示");
                             alert.setMessage("确认删除此图片？");
 
-                            alert.setButton(DialogInterface.BUTTON_NEGATIVE,"取消", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog , int which) {
-                                    Toast.makeText(MyToolAcyivity.this,"删除失败",Toast.LENGTH_SHORT).show();
-                                }
-                            });
+                            alert.setButton(DialogInterface.BUTTON_NEGATIVE, "取消",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Toast.makeText(MyToolActivity.this, "删除失败",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
 
-                            alert.setButton(DialogInterface.BUTTON_POSITIVE,"确定", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog , int which) {
-                                    imageId.remove(position);
-                                    adapter.notifyDataSetChanged();
-                                    gridView.setAdapter(adapter);
-                                    Toast.makeText(MyToolAcyivity.this,"删除成功",Toast.LENGTH_SHORT).show();
-                                }
-                            });
+                            alert.setButton(DialogInterface.BUTTON_POSITIVE, "确定",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            imageId.remove(position);
+                                            adapter.notifyDataSetChanged();
+                                            gridView.setAdapter(adapter);
+                                            Toast.makeText(MyToolActivity.this, "删除成功",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
                             alert.show();
                             return true;
                         }
@@ -115,26 +120,25 @@ public class MyToolAcyivity<liv> extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true){
-                    mProgressStatus = dowork();
+                while (true) {
+                    mProgressStatus = doWork();
                     Message m = new Message();
-                    if(mProgressStatus<100){
-                        m.what = 0x111;
+                    if (mProgressStatus < PROGRESS_MAX_VALUE) {
+                        m.what = MSG_UPDATE_PROGRESS;
                         mHandler.sendMessage(m);
-                    }else{
-                        m.what = 0x110;
+                    } else {
+                        m.what = MSG_FINISH_PROGRESS;
                         mHandler.sendMessage(m);
-
                         break;
                     }
                 }
             }
 
-            private int dowork(){
-                mProgressStatus+=Math.random()*10;
+            private int doWork() {
+                mProgressStatus += Math.random() * 10;
                 try {
-                    Thread.sleep(200);
-                }catch (InterruptedException e){
+                    Thread.sleep(PROGRESS_DELAY_VALUE);
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 return mProgressStatus;
